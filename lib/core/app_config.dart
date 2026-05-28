cf1208a85d156286f60dc2a686398c150f8efa47
Polechka <tereshchenkopolina0@mail.ru>
2026-05-28 13:07:46 +0500

    stable app

--- lib/core/app_config.dart
+++ lib/core/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   const AppConfig._();
 
-  static const supabaseUrl = 'https://xayghqwulxktwvikeqqg.supabase.co';
+  static const supabaseUrl = '';
   static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhheWdocXd1bHhrdHd2aWtlcXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MTk1NDEsImV4cCI6MjA5MTA5NTU0MX0.YvhCFfHazmL_v1JZYMFdRn-HvAt00GHZHTX5UIzpRIw';
+      '';
 }
